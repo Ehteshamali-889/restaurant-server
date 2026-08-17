19b09c6a874946a416e2d8461b707d8604e05696
dmustoo <ehtesham7643@gmail.com>
2026-08-17 22:02:02 +0500

    cors fix

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://restopropos.netlify.app', 'http://localhost:3000'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
